3a8d0288e85cd22020ea0b4642033c72d62d1ce7
pi <pi@local>
2019-04-13 17:35:31 -0500

    add file

---
+++ daily_tr.php
@@ -0,0 +1,137 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+require_once "config.php";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee</title>
+    <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css">
+    <script src="/jquery/dist/jquery.min.js"></script>
+    <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        .page-header h2{
+            margin-top: 0;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+        .dropdown {
+            position: relative;
+            display: inline-block;
+            vertical-align: middle;
+            padding-right: 0px;
+        }
+        .dropdown-menu {
+      font-size: 20px;
+        }
+        .btn-success {
+            margin-right: 5px;
+        }
+        .table tbody tr {
+            font-size: 14px;
+            text-align: left;
+
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
+</head>
+<body>
+    <div class="container">
+        <div class="col-sm-4"> </div>
+        <div class="col-sm-4">
+            <div class="page-header clearfix">
+                <h2 class="pull-left">Daily </h2>
+                
+                <div class="pull-right">
+                    <div class="dropdown">
+                        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
+                            <i class="glyphicon glyphicon-list" style="font-size:12px;"></i>
+                            <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu pull-right">
+                          <li><a href="welcome.php">Home</a></li>
+                          <li><a href="index.php">Employee</a></li>
+                          <li><a href="input.php">Add Record</a></li>
+                          <li><a href="daily_tr.php">Daily Transaction</a></li>
+                          <li><a href="reset-password.php">Reset Password</a></li>
+                          <li><a href="logout.php">Log Out</a></li>
+                      </ul>
+                  </div>
+              </div>
+              
+              <a href="input.php" class="btn btn-success pull-right">+</a>
+          </div>
+          <?php
+                    // Include config file
+          require_once "config.php";
+          $session_user= $_SESSION["username"];
+
+                    // Attempt select query execution
+          $sql = "SELECT * FROM booki_tbl INNER JOIN users ON booki_tbl.id = users.id WHERE users.username ='$session_user'";
+          if($result = mysqli_query($link, $sql)){
+            if(mysqli_num_rows($result) > 0){
+                echo "<table class='table table-bordered table-striped'>";
+                echo "<thead>";
+                echo "<tr>";
+                echo "<th>#</th>";
+                echo "<th>Name</th>";
+                echo "<th>$$$</th>";
+                echo "<th>Tip</th>";
+                echo "<th>Sup</th>";
+                echo "<th>Sum</th>";
+                echo "<th>Action</th>";
+                echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+                while($row = mysqli_fetch_array($result)){
+                    echo "<tr>";
+                    echo "<td>" . $row['booki_id'] . "</td>";
+                    echo "<td>" . $row['booki_employee'] . "</td>";
+                    $total_money = $row['booki_cash'] + $row['booki_card'];
+                    echo "<td>" . $total_money . "</td>";
+                    echo "<td>" . $row['booki_tip'] . "</td>";
+                    echo "<td>" . $row['booki_supply'] . "</td>";
+                    $total_cash = $total_money + $row['booki_tip'] - $row['booki_supply'];
+                    echo "<td>" . $total_cash . "</td>";
+                    echo "<td>";
+                    echo "<a href='update_daily.php?id=". $row['booki_id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                echo "</tbody>";                            
+                echo "</table>";
+                            // Free result set
+                mysqli_free_result($result);
+            } else{
+                echo "<p class='lead'><em>No records were found.</em></p>";
+            }
+        } else{
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+        }
+
+                    // Close connection
+        mysqli_close($link);
+        ?>
+    </div>
+    <div class="col-sm-4"></div>
+</div>        
+
+</body>
+</html>
---
+++ update_daily.php
@@ -0,0 +1,199 @@
+<?php
+// Include config file
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$name = $cassh = $card = $tip = $supply = "";
+$name_err = $cassh_err = $card_err = $tip_err = $supply_err = "";
+ 
+// Processing form data when form is submitted
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+    // Get hidden input value
+    $id = $_POST["id"];
+    
+    // Validate name
+    $name = trim($_POST["name"]);
+    $cassh = trim($_POST["cassh"]);
+    $card = trim($_POST["card"]);
+    $tip = trim($_POST["tip"]);
+    $supply = trim($_POST["supply"]);
+    
+    if(empty($cassh)){
+        $cash =0 ;
+    }
+    if(empty($card)){
+        $card =0 ;
+    }
+    if(empty($tip)){
+        $tip =0 ;
+    }
+    if(empty($supply)){
+        $supply =0 ;
+    }
+
+
+    
+    // Check input errors before inserting in database
+    if(empty($name_err)){
+        // Prepare an update statement
+        $sql = "UPDATE booki_tbl SET booki_cash = ?, booki_card = ?, booki_tip = ?, booki_supply = ? WHERE booki_id = ?";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "iiiii", $param_cash, $param_card, $param_tip, $param_supply, $param_id);
+            
+            
+            
+            
+            
+            $param_cash = $cassh;
+            $param_card = $card;
+            $param_tip = $tip;
+            $param_supply = $supply;
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records updated successfully. Redirect to landing page
+                header("location: update_daily.php");
+                exit();
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // Check existence of id parameter before processing further
+    if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+        // Get URL parameter
+        $id =  trim($_GET["id"]);
+        
+        // Prepare a select statement
+        $sql = "SELECT * FROM booki_tbl WHERE booki_id = ?";
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "i", $param_id);
+            
+            // Set parameters
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                $result = mysqli_stmt_get_result($stmt);
+    
+                if(mysqli_num_rows($result) == 1){
+                    /* Fetch result row as an associative array. Since the result set contains only one row, we don't need to use while loop */
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                    
+                    // Retrieve individual field value
+                    $name = $row["booki_employee"];
+                    $card = $row["booki_card"];
+                    $cassh = $row["booki_cash"];
+                    $tip = $row["booki_tip"];
+                    $supply = $row["booki_supply"];
+                } else{
+                    // URL doesn't contain valid id. Redirect to error page
+                    header("location: error.php");
+                    exit();
+                }
+                
+            } else{
+                echo "Oops! Something went wrong. Please try  again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+        
+        // Close connection
+        mysqli_close($link);
+    }  else{
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: daily_tr.php");
+        exit();
+    }
+
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css">
+    <script src="/jquery/dist/jquery.min.js"></script>
+    <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: left; }
+    </style>
+</head>
+<body>
+    <div class="container">
+              <div class="row">
+                <div class="col-sm-4">
+
+                </div>
+                <div class="col-sm-4">
+                    <div class="page-header">
+                        <h2>Update Record</h2>
+                    </div>
+                    <p>Please edit the input values and submit to update the record.</p>
+                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" readonly="readonly">
+                            <span class="help-block"><?php echo $name_err;?></span>
+                        </div>
+                         <div class="form-group <?php echo (!empty($cassh_err)) ? 'has-error' : ''; ?>">
+                            <label>Cash</label>
+                            <input type="text" name="cassh" class="form-control" value="<?php echo $cassh; ?>">
+                            <span class="help-block"><?php echo $cassh_err;?></span>
+                        </div>
+                         <div class="form-group <?php echo (!empty($card_err)) ? 'has-error' : ''; ?>">
+                            <label>Card</label>
+                            <input type="text" name="card" class="form-control" value="<?php echo $card; ?>">
+                            <span class="help-block"><?php echo $card_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($tip_err)) ? 'has-error' : ''; ?>">
+                            <label>Tip</label>
+                            <input type="text" name="tip" class="form-control" value="<?php echo $tip; ?>">
+                            <span class="help-block"><?php echo $tip_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($supply_err)) ? 'has-error' : ''; ?>">
+                            <label>Supply</label>
+                            <input type="text" name="supply" class="form-control" value="<?php echo $supply; ?>">
+                            <span class="help-block"><?php echo $supply_err;?></span>
+                        </div>
+
+                        <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                        <input type="submit" class="btn btn-primary" value="Save">
+                        <a href="daily_tr.php" class="btn btn-default">Cancel</a>
+                    </form>
+                </div>
+                <div class="col-sm-4"></div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
