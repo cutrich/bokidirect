b7309da26e20d329353afa4e47c9b8ae2ba1aaf9
pi <pi@local>
2019-04-21 19:26:26 -0500

    add index

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 
 session_start();
@@ -6,99 +7,68 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
-
 require_once "config.php";
 
+
 ?>
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Employee</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css">
-    <script src="/jquery/dist/jquery.min.js"></script>
-    <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();   
-        });
-    </script>
-    <style type="text/css">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Transaction</title>
+ <link rel="stylesheet" href="/css/style.css">
+ <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css">
 
-.page-header h2{
-    margin-top: 0;
-}
-    </style>
+ <script src="/jquery/dist/jquery.min.js"></script>
+ <link rel="stylesheet" href="/bootstrap/dist/js/jquery.bootgrid.css" />
+ <script src="/bootstrap/dist/js/jquery.bootgrid.min.js"></script>
+ <style type="text/css">
+    .page-header h2{
+        margin-top: 0;
+    }
+</style>
+
+
 </head>
 <body>
+    <div class="container">
 
-            <div class="page-header clearfix">
-                <h2 class="pull-left">Employees </h2>
-                
-                <div class="pull-right">
-                    <div class="dropdown">
-                        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
-                            <i class="glyphicon glyphicon-list" style="font-size:12px;"></i>
-                            <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu pull-right">
-                          <li><a href="welcome.php">Home</a></li>
-                          <li><a href="index.php">Employee</a></li>
-                          <li><a href="daily_tr.php">Transaction</a></li>
-                          <li><a href="reset-password.php">Reset Password</a></li>
-                          <li><a href="logout.php">Log Out</a></li>
-                      </ul>
-                  </div>
-              </div>
-              
-              <a href="create.php" class="btn btn-success pull-right">+</a>
-          </div>
-          <?php
-                    // Include config file
-          require_once "config.php";
+        <h2 >Transaction</h2>
+        
 
-                    // Attempt select query execution
-          $sql = "SELECT * FROM ".$_SESSION["username"]."_employee";
-          if($result = mysqli_query($link, $sql)){
-            if(mysqli_num_rows($result) > 0){
-                echo "<table class='table table-bordered table-striped'>";
-                echo "<thead>";
-                echo "<tr>";
-                echo "<th>#</th>";
-                echo "<th>Name</th>";
-                echo "<th>Action</th>";
-                echo "</tr>";
-                echo "</thead>";
-                echo "<tbody>";
-                while($row = mysqli_fetch_array($result)){
-                    echo "<tr>";
-                    echo "<td>" . $row['id'] . "</td>";
-                    echo "<td>" . $row['employee'] . "</td>";
-                    echo "<td>";
-                    echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
-                    echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
-                    echo "</td>";
-                    echo "</tr>";
-                }
-                echo "</tbody>";                            
-                echo "</table>";
-                            // Free result set
-                mysqli_free_result($result);
-            } else{
-                echo "<p class='lead'><em>No records were found.</em></p>";
-            }
-        } else{
-            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-        }
+            <div class="well clearfix">
+                <div class="pull-right"><button type="button" class="btn btn-xs btn-primary" id="command-add" data-row-id="0">
+                    <span class="glyphicon glyphicon-plus"></span> Record</button>
+                </div>
+                </div>
+                <table id="employee" data-toggle="bootgrid" class="table table-condensed table-hover table-striped" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th data-column-id="emp_id" data-type="numeric">Id</th>
+                            <th data-column-id="emp_name">Name</th>
+                            <th data-column-id="emp_cash">Cash</th>
+                            <th data-column-id="emp_card">Card</th>
+                            <th data-column-id="emp_tip">Tip</th>
+                            <th data-column-id="emp_supply">Supply</th>
+                            <th data-column-id="emp_date">Date</th>
+                        </tr>
+                    </thead>
+                </table>
 
-                    // Close connection
-        mysqli_close($link);
-        ?>
-           
+        </div>
 
-</body>
-</html>
+
+    </body>
+    </html>
+    
+
+<script type="text/javascript">
+
+	
+    $( document ).ready(function() {
+        $("#employee").bootgrid({
+            ajax: true,
+            url: "fetch_data.php"
+        });
+    });
+</script>
